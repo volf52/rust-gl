7d40ceca4706a1e079a3f0b59cad019b328c5e70
Arslan <rslnkrmt2552@gmail.com>
2021-11-08 10:04:48 +0300

    Move Attribs to DisplayObject
    - BufferInfo wasn't holding any data other than Attribs, and consequently was acting merely as a proxy for Attribs. And so it has been removed.
    - Attribs is moved to DisplayObject as that's where the render/draw call is being made, for which we directly need the Attribs struct.

--- src/display/display_object.rs
+++ src/display/display_object.rs
@@ -1,7 +1,8 @@
-use crate::display::buffers::BufferInfo;
 use crate::display::shader_program::ShaderProgram;
 use crate::graphics::shape::Shape;
 use web_sys::WebGl2RenderingContext;
+
+use super::attribs::Attribs;
 
 // #[derive(Debug, Clone)]
 // pub struct DisplayObject<'a, T: Shape + Clone> {
@@ -31,21 +32,30 @@ use web_sys::WebGl2RenderingContext;
 pub struct DisplayObject {
     ctx: WebGl2RenderingContext,
     shape: Box<dyn Shape>,
+    attribs: Attribs,
 }
 
 impl DisplayObject {
     pub fn new(ctx: &WebGl2RenderingContext, shape: Box<dyn Shape>) -> DisplayObject {
-        // TODO: move buffer info/attribs here
         let ctx = ctx.clone();
-        DisplayObject { ctx, shape }
+        let attribs = Attribs::new(&ctx, &*shape);
+
+        DisplayObject {
+            ctx,
+            shape,
+            attribs,
+        }
     }
     pub fn draw(&self) {
         let gl_program = ShaderProgram::new(&self.ctx);
-        let buffer_info: BufferInfo = BufferInfo::new(&self.ctx, &*self.shape);
+
         // TODO: Calculate vertices, transformation mat
-        buffer_info.set_attributes_and_buffers(&gl_program);
+        self.attribs.set_attributes(&gl_program);
         // TODO: Set uniforms
+
         let mode = self.shape.mode();
-        self.ctx.draw_arrays(mode, 0, self.shape.vertex_count());
+        let vert_count = self.shape.vertex_count();
+
+        self.ctx.draw_arrays(mode, 0, vert_count);
     }
 }
--- src/display/mod.rs
+++ src/display/mod.rs
@@ -1,4 +1,3 @@
 mod attribs;
-mod buffers;
 pub mod display_object;
 mod shader_program;
