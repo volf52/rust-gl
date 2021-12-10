929eb44f71cf4f0911463683c07eedb0ba531487
Quirk23 <rehan.qureshi@carbonteq.com>
2021-12-10 19:00:58 +0000

    Used the typer lib to generate text textures.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use std::{cell::RefCell, rc::Rc};
 
 use graphics::Container;
+use textures::ab_text::test_tex2;
+use textures::texture_text::test_tex;
+use textures::typer_text::text_typer;
 use utils::{console_error, console_log};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -11,7 +14,7 @@ use crate::core::application::{Application, CanvasDimensions};
 use crate::graphics::shapes::{
     Circle, IrregularPolygon, Rectangle, RegularPolygon, Shape, Triangle,
 };
-
+use webgl2_glyph_atlas::{Font, Renderer};
 mod core;
 mod display;
 mod graphics;
@@ -83,20 +86,29 @@ pub fn main() -> Result<(), JsValue> {
     let mut app = Application::new(&context, dims);
 
     let _tex = app.tex_from_img("../assets/test.jpg");
-    let text: WebGlTexture = app.text_texture("test", "sans-serif", 36, "white", 20.0, 0.0);
-    let c = Circle::new(0, 0, 100.0, &text);
+    let _text: WebGlTexture = app.text_texture("test", "sans-serif", 40, "white", 20.0, 0.0);
+
+    let text = r##"
+            <block width="100" x="0" y="30" text-align="center">
+                <s font-size="12" color="#FFFFFF" line-height="1.2"><s color="#FF0000" font-size="24">Paprika</s> is an incredibly good movie.</s>
+            </block>"##;
+
+    let text_texture = text_typer(&context, text);
+    let c = Circle::new(0, 0, 100.0, &text_texture);
     c.translate(-150.0, 75.0);
 
     app.add_shape(&c);
     let tr = Triangle::new_at_origin(100.0, &red);
     container.add_shape(&tr);
-
     app.add_container(&container);
-
+    //   app.render();
     render_loop(move || {
         app.render();
         tr.rotate_deg(5.0);
         c.rotate_deg(1.0);
+        let mut renderer = Renderer::new(&context).unwrap();
+        renderer.queue_text("Hello ", &Font::new("sans-serif", 40), 0., 0.);
+        renderer.draw().unwrap();
     });
 
     Ok(())
